27573237ca96062e802c60e1820455a5a5849403
kanishk2404 <saraswatkanishk24.com>
2025-07-26 02:15:14 +0530

    fixed db issue

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,14 @@
-const {Sequelize}  =  require('sequelize');
+const path = require('path');
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const dbDir = path.join(__dirname, 'db');
+if (!fs.existsSync(dbDir)) {
+    fs.mkdirSync(dbDir);
+}
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: 'database.sqlite',
+    storage: path.join(dbDir, 'database.sqlite'),
     logging: false
 });
 
-module.exports = sequelize;
+module.exports = sequelize;  
